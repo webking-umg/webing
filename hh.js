bd7bea8cc8f19adf5fc02318d31e7475f6e2a588
webking-umg <76899490+webking-umg@users.noreply.github.com>
2021-01-04 11:30:22 +0530

    Create hh.js

---
+++ hh.js
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script data-ad-client="ca-pub-7816710903947692" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+</head>
+</html>
