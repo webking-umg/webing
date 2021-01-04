27224d247fdc3abf358ac6933266e15922054b05
webking-umg <76899490+webking-umg@users.noreply.github.com>
2021-01-04 11:34:39 +0530

    Delete hh.js

--- hh.js
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-<script data-ad-client="ca-pub-7816710903947692" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-</head>
-</html>
