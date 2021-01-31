d8fa9f31ce2eaf7b3da0c61801d998134839ffbd
webking-umg <76899490+webking-umg@users.noreply.github.com>
2021-01-31 10:52:34 +0530

    Delete ember.js

--- ember.js
+++
@@ -1,7 +0,0 @@
-<html>
-   <head>
-   <script data-ad-client="ca-pub-7816710903947692" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-   </head>
-   <body>    
-   </body>
-</html>
