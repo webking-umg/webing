0607c668e19256afb87ba2e9ecf02ef440acda52
webking-umg <76899490+webking-umg@users.noreply.github.com>
2021-01-31 10:44:31 +0530

    Update ember.js

--- ember.js
+++ ember.js
@@ -1,9 +1,7 @@
 <html>
-   <body>   
-      <script language = "javascript" type = "text/javascript">
-         <!--
-            document.write("Hello World!")
-         //-->
-      </script>      
+   <head>
+   <script data-ad-client="ca-pub-7816710903947692" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+   </head>
+   <body>    
    </body>
 </html>
